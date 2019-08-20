f84ff4e93a886c54b373a65c2d06cb9e9f8e52c5
Maverick Adolfo <merkonical@gmail.com>
2019-08-20 21:56:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 def find_the_cheese(cheddar_cheese) # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  cheddar_cheese.find do |cheddar|
 end
