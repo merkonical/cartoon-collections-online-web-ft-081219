ce1f9b512aef743cb14cf4b26629ad023a38f600
Maverick Adolfo <merkonical@gmail.com>
2019-08-20 21:48:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves) # code an argument here
+  
+  dwarves.each_with_index do |dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  end # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls) # code an argument here
+  planeteer_calls.map { |call| call.capitalize + "!" }
 end
 
 def long_planeteer_calls# code an argument here
