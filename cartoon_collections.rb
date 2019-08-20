11733f3f8224d148b427142449287a4198188685
Maverick Adolfo <merkonical@gmail.com>
2019-08-20 21:53:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def summon_captain_planet(planeteer_calls) # code an argument here
 end
 
 def long_planeteer_calls(calls_long) # code an argument here
-  calls_long.any? { |call| call.include?
+  calls_long.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
